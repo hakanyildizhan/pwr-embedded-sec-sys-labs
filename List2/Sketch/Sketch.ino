71da4b6fbd2c4cc3fbbfdea5414c87ee0d672032
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-10-31 18:57:16 +0100

    Role

--- List2/Sketch/Sketch.ino
+++ List2/Sketch/Sketch.ino
@@ -4,13 +4,14 @@
 A51Cipher cipherLib;
 STIR commLib(0,0);
 bool keyStream[228];
+const uint8_t ROLE = 0x1;
 
 void setup()
 {
     cipherLib = A51Cipher();
     cipherLib.createCipherKey(keyStream);
     STIRConfig config(0, 0, 0, 9, 3, ProcessIncoming::WRITETOSERIAL);
-    commLib = STIR(config);
+    commLib = STIR(config, ROLE);
     commLib.beginListen();
 }
 
